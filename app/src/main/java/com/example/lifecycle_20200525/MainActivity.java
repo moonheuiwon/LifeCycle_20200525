37067ada4b563e9baeb39afb465d36a140ad814d
moonheuiwon <wonheuimoon@gmail.com>
2020-05-25 14:56:03 +0900

    버튼 추가 두번째 엑티비티로 이동

--- app/src/main/java/com/example/lifecycle_20200525/MainActivity.java
+++ app/src/main/java/com/example/lifecycle_20200525/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.lifecycle_20200525;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import com.example.lifecycle_20200525.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setupEvents();
         setValues();
 
@@ -20,6 +27,13 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        binding.moveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(mContext, SecondsActivity.class);
+                startActivity(myIntent);
+            }
+        });
 
     }
 
